8c52835a8223d33949e3d76028b4e4f9f1a4a2ee
zhangliucheng <414616504@qq.com>
2019-08-30 19:19:05 +0800

    update

--- app/src/main/java/com/tech/playinapi/Constants.java
+++ app/src/main/java/com/tech/playinapi/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static final String HOST = "https://api.playinads.com/webview/available";
 
     public static final String TAG = "PLAYIN";
-    public static final String ADID = "e6QwJImi";
+    public static final String ADID = "tSKD2fYg";
 
 }
